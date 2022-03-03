d57b0c40d6428b74fc081887222bb6109bb7e208
Ming-Chiang Chang <mc2663@cornell.edu>
2022-03-03 15:00:10 -0500

    Fix peak profile

---
+++ scripts/test.jl
@@ -0,0 +1,63 @@
+using CrystalShift
+using CrystalShift: CrystalPhase, optimize!, evaluate, get_free_params, Lorentz
+using CrystalShift: newton!, get_free_lattice_params, get_fraction, PseudoVoigt
+using CrystalShift: new_evaluate!, evaluate!,get_param_nums
+
+using LinearAlgebra
+using Random: rand
+using BenchmarkTools
+using Plots
+
+verbose = false
+residual_tol = 0.1 # tolerance for residual norm after optimization
+maxiter = 512 # appears to be required for phase combinations in particular
+
+# Global
+std_noise = 0.1
+mean_θ = [1., 1, .2]
+std_θ = [.02, 1., 1.]
+# newton_lambda = 1e-2 TODO: make this passable to the newton optimization
+
+function synthesize_data(cp::CrystalPhase, x::AbstractVector)
+    params = get_free_lattice_params(cp)
+    interval_size = 0.025
+    scaling = (interval_size.*rand(size(params, 1),) .- interval_size/2) .+ 1
+    @. params = params*scaling
+    params = [params..., 1., 0.2, 0.5*rand(1)...]
+    r = zero(x)
+    evaluate!(r, cp, params, x)
+    normalization = maximum(r)
+    params[end-1] /= normalization
+    return r/normalization, params
+end
+
+test_path = "data/Ta-Sn-O/sticks.csv" # when ]test is executed pwd() = /test
+f = open(test_path, "r")
+
+if Sys.iswindows()
+    s = split(read(f, String), "#\r\n")
+else
+    s = split(read(f, String), "#\n")
+end
+
+cs = CrystalPhase.(String.(s[1:end-1]), (0.1,), (PseudoVoigt(0.5),))
+x = collect(8:.1:60)
+y = zero(x)
+
+test, params = synthesize_data(cs[1], x)
+test = evaluate!(zero(x), cs[1], params, x)
+c = optimize!(cs[1], x, test, std_noise, mean_θ, std_θ;
+                  method =LM,
+                  maxiter = maxiter, regularization = false, verbose=false)
+
+plt = plot(x, test)
+plot!(x, evaluate!(y, c, x))
+display(plt)
+
+# t = rand(get_param_nums(cs[1]))
+# tt = rand(get_param_nums(cs[1:3]))
+# println("single phase")
+# @btime evaluate!(y, cs[1], t, x)
+# println("3 phases")
+# @btime evaluate!(y, cs[1:3], tt, x)
+# println("")
--- test/optimize.jl
+++ test/optimize.jl
@@ -6,7 +6,7 @@ using CrystalShift: newton!, get_free_lattice_params, get_fraction
 using LinearAlgebra
 using Random: rand
 using Test
-# using BenchmarkTools
+using BenchmarkTools
 
 verbose = false
 residual_tol = 0.1 # tolerance for residual norm after optimization
@@ -18,7 +18,7 @@ mean_θ = [1., 1, .2]
 std_θ = [.02, 1., 1.]
 # newton_lambda = 1e-2 TODO: make this passable to the newton optimization
 
-test_path = "../data/Ta-Sn-O/sticks.csv" # when ]test is executed pwd() = /test
+test_path = "data/Ta-Sn-O/sticks.csv" # when ]test is executed pwd() = /test
 f = open(test_path, "r")
 
 if Sys.iswindows()
@@ -35,7 +35,7 @@ function test_optimize(cp::CrystalPhase, x::AbstractVector,
     y, sol = synthesize_data(cp, x)
     c = optimize!(cp, x, y, std_noise, mean_θ, std_θ;
                   method = method,
-                  maxiter = maxiter, regularization = true)
+                  maxiter = maxiter, regularization = true, verbose=false)
 
     if verbose
         println(c[1].cl)
@@ -98,84 +98,84 @@ function test_multiphase_optimize(cps::AbstractVector{<:CrystalPhase},
     norm(c.(x).-y)
 end
 
-@time test_optimize(cs[10], x, LM, verbose)
-@time test_optimize(cs[10], x, Newton, false)
-@time test_multiphase_optimize(cs, x, 2, LM, "LS", verbose)
+@btime test_optimize(cs[10], x, LM, verbose)
+# @btime test_optimize(cs[10], x, Newton, false)
+@btime test_multiphase_optimize(cs, x, 2, LM, "LS", verbose)
 
 
 # test fails when the lattice parameters shift too much
-@testset "Single phase with shift LM test" begin
-    correct_counts = 0
-    for (idx, cp) in enumerate(cs)
-        verbose && println(idx)
-        if test_optimize(cp, x, LM, verbose) < 0.1
-            correct_counts += 1
-        end
-    end
-    @test correct_counts >= size(cs, 1) - 1
-end
-
-@testset "Single phase with shift newton test" begin
-    correct_counts = 0
-    for (idx, cp) in enumerate(cs)
-        verbose && println(idx)
-        if  test_optimize(cp, x, Newton, false) < 0.1
-            correct_counts += 1
-        end
-    end
-    @test correct_counts >= size(cs, 1) - 1
-end
-
-# @testset "Single phase with shift bfgs test" begin
+# @testset "Single phase with shift LM test" begin
 #     correct_counts = 0
 #     for (idx, cp) in enumerate(cs)
 #         verbose && println(idx)
-#         if  test_optimize(cp, x, bfgs, true) < 0.1 # FIXME: Sometimes sin(x) goes to infinite (This only happens when doing test)
-#             correct_counts += 1                    # FIXME: dx goes to 0.0 when std_noise goes to 1e-2
-#         end
-#     end
-#     @test correct_counts >= size(cs, 1) - 1
-# end
-
-# @testset "Single phase with shift lbfgs test" begin
-#     correct_counts = 0
-#     for (idx, cp) in enumerate(cs)
-#         verbose && println(idx)
-#         if  test_optimize(cp, x, l_bfgs, true) < 0.1
+#         if test_optimize(cp, x, LM, verbose) < 0.1
 #             correct_counts += 1
 #         end
 #     end
 #     @test correct_counts >= size(cs, 1) - 1
 # end
 
-@testset "Multiple phases with shift test" begin
-    correct_counts = 0
-    for _ in 1:5
-        if test_multiphase_optimize(cs, x, 2, LM, "LS", verbose) < 0.1
-            correct_counts += 1
-        end
-    end
-    @test correct_counts >= 3
-end
+# @testset "Single phase with shift newton test" begin
+#     correct_counts = 0
+#     for (idx, cp) in enumerate(cs)
+#         verbose && println(idx)
+#         if  test_optimize(cp, x, Newton, false) < 0.1
+#             correct_counts += 1
+#         end
+#     end
+#     @test correct_counts >= size(cs, 1) - 1
+# end
 
-# @testset "Multiple phases with shift newton KL test" begin # call it a success if 4/5 cases passed
+# # @testset "Single phase with shift bfgs test" begin
+# #     correct_counts = 0
+# #     for (idx, cp) in enumerate(cs)
+# #         verbose && println(idx)
+# #         if  test_optimize(cp, x, bfgs, true) < 0.1 # FIXME: Sometimes sin(x) goes to infinite (This only happens when doing test)
+# #             correct_counts += 1                    # FIXME: dx goes to 0.0 when std_noise goes to 1e-2
+# #         end
+# #     end
+# #     @test correct_counts >= size(cs, 1) - 1
+# # end
+
+# # @testset "Single phase with shift lbfgs test" begin
+# #     correct_counts = 0
+# #     for (idx, cp) in enumerate(cs)
+# #         verbose && println(idx)
+# #         if  test_optimize(cp, x, l_bfgs, true) < 0.1
+# #             correct_counts += 1
+# #         end
+# #     end
+# #     @test correct_counts >= size(cs, 1) - 1
+# # end
+
+# @testset "Multiple phases with shift test" begin
 #     correct_counts = 0
 #     for _ in 1:5
-#         if test_multiphase_optimize(cs, x, 2, Newton, "KL", verbose) < 0.1
+#         if test_multiphase_optimize(cs, x, 2, LM, "LS", verbose) < 0.1
 #             correct_counts += 1
 #         end
 #     end
 #     @test correct_counts >= 3
 # end
 
-@testset "Multiple phases with shift newton least sqaure test" begin # call it a success if 4/5 cases passed
-    correct_counts = 0
-    for _ in 1:5
-        if test_multiphase_optimize(cs, x, 2, Newton, "LS", verbose) < 0.1
-            correct_counts += 1
-        end
-    end
-    @test correct_counts >= 3
-end
+# # @testset "Multiple phases with shift newton KL test" begin # call it a success if 4/5 cases passed
+# #     correct_counts = 0
+# #     for _ in 1:5
+# #         if test_multiphase_optimize(cs, x, 2, Newton, "KL", verbose) < 0.1
+# #             correct_counts += 1
+# #         end
+# #     end
+# #     @test correct_counts >= 3
+# # end
+
+# @testset "Multiple phases with shift newton least sqaure test" begin # call it a success if 4/5 cases passed
+#     correct_counts = 0
+#     for _ in 1:5
+#         if test_multiphase_optimize(cs, x, 2, Newton, "LS", verbose) < 0.1
+#             correct_counts += 1
+#         end
+#     end
+#     @test correct_counts >= 3
+# end
 
 end # module
