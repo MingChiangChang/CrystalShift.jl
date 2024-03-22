2fe19a934b326e42fd70925f0469b9d6f31b017e
Ming-Chiang Chang <mc2663@cornell.edu>
2024-03-22 16:07:21 -0400

    Fix precomile bug

--- src/CrystalShift.jl
+++ src/CrystalShift.jl
@@ -14,6 +14,7 @@ using FastPow
 using StatsBase
 using Printf
 using PrecompileTools
+using Random
 # using PyCall
 
 # TODO: Update export list
@@ -70,13 +71,26 @@ include("optimizationsettings.jl")
 include("optimize.jl")
 
 @setup_workload begin
-    std_noise = 1.
-    mean_θ = [1., .5, .2]
-    std_θ = [.05, 2., 1.]
-    test_path = "data/Ta-Sn-O/sticks.csv"
-    x = collect(8:.1:60)
+    std_noise = 1.0
+    mean_θ = [1.0, 0.5, 0.2]
+    std_θ = [0.05, 2.0, 1.0]
+    x = collect(8:0.1:60)
     y = zero(x)
     K = CovarianceFunctions.EQ()
+    lps = [[1., 1., 1., pi/2, pi/2, pi/2],
+            [1., 1., 2., pi/2, pi/2, pi/2],
+            [1., 2., 3., pi/2, pi/2, pi/2],
+            [1., 2., 3., pi/2, pi/2*0.98, pi/2],
+            [1., 1., 3., pi/2, pi/2, 2*pi/3],
+            [4., 5., 6., 2pi/3, 3pi/4, 5pi/6]]
+    hkls = [[[1, 1, 1]],
+            [[2, 3, 4]],
+            [[5, 6, 7]],
+            [[8, 9, 10]],
+            [[11, 12, 13]],
+            [[1, 3, 4]]]
+    peak_heights = [rand(1) for _ in 1:length(hkls) ]
+    _names = ["Cubic", "Tetragonal", "Orthorhombic", "Monoclinic", "Hexagonal", "Triclinic"]
 
     @compile_workload begin
         Gauss()
@@ -85,34 +99,25 @@ include("optimize.jl")
         PseudoVoigt(0.5)
         bg = BackgroundModel(x, K, 10)
 
-        f = open(test_path, "r")
-        CrystalPhase(f, 0.1, FixedPseudoVoigt(0.5) )
+        cs = CrystalPhase.(lps, hkls, peak_heights, _names, collect(1:length(lps)), (0.1, ), (FixedApproxPseudoVoigt(0.5),))
 
-        f = open(test_path, "r")
-        if Sys.iswindows()
-            s = split(read(f, String), "#\r\n")
-        else
-            s = split(read(f, String), "#\n")
-        end
-        cs = CrystalPhase.(String.(s[1:end-1]), (0.1,), (FixedPseudoVoigt(0.5),));
-        
         evaluate!(y, cs[1], [16.9, 4.86, 5.55, 1.5929519726520256, 0.687, 0.2, 0.5], x)
         optimize!(PhaseModel(cs[1:3]), x, y, std_noise, mean_θ, std_θ;
-                  method = LM, maxiter = 32,
-                  optimize_mode=Simple, em_loop_num=1,
-                  regularization = true, verbose = false)
+            method=LM, maxiter=32,
+            optimize_mode=Simple, em_loop_num=1,
+            regularization=true, verbose=false)
         optimize!(PhaseModel(cs[1:3]), x, y, std_noise, mean_θ, std_θ;
-                  method = LM, maxiter = 32,
-                  optimize_mode=EM, em_loop_num=1,
-                  regularization = true, verbose = false)
+            method=LM, maxiter=32,
+            optimize_mode=EM, em_loop_num=1,
+            regularization=true, verbose=false)
         optimize!(cs[1:3], x, y, std_noise, mean_θ, std_θ;
-                  method = LM, maxiter = 32,
-                  optimize_mode=Simple, em_loop_num=1,
-                  regularization = true, verbose = false)
+            method=LM, maxiter=32,
+            optimize_mode=Simple, em_loop_num=1,
+            regularization=true, verbose=false)
         optimize!(PhaseModel(cs[1:3], nothing, bg), x, y, std_noise, mean_θ, std_θ;
-                  method = LM, maxiter = 32,
-                  optimize_mode=Simple, em_loop_num=1,
-                  regularization = true, verbose = false)
+            method=LM, maxiter=32,
+            optimize_mode=Simple, em_loop_num=1,
+            regularization=true, verbose=false)
         # Not doing this because it takes a long time, uncomment if you need to use Newton
         # optimize!(PhaseModel(cs[1:3], nothing, bg), x, y, std_noise, mean_θ, std_θ; 
         #           method = Newton, maxiter = 32,
